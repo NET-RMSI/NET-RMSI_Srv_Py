e14ce3771dfdb0e787e08399da606f19337bbd32
Kitty Rose Hamilton <kittyrhamilton@gmail.com>
2022-08-30 12:53:05 +0100

    Update TCPServer.py

--- src/TCPServer.py
+++ src/TCPServer.py
@@ -1,1 +1,15 @@
+from multiprocessing.connection import wait
 import socket
+
+
+PORT = 13062
+tcpsrv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+tcpsrv.bind((socket.gethostname(), PORT))
+tcpsrv.listen(1)
+tcpcli, address = tcpsrv.accept()
+
+
+while True:
+   print ("Connected")
+   wait(1000)
+    
