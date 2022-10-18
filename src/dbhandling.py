10408ed365e874d28954b52d4b856af4187f846a
Kitty Rose Hamilton <kittyrhamilton@gmail.com>
2022-10-18 12:43:12 +0100

    Update dbhandling.py

--- src/dbhandling.py
+++ src/dbhandling.py
@@ -2,15 +2,17 @@ import pymongo
 from _global import *
 from LoggerModule import *
 
-def DBHANDLINGINIT():
+def DBHANDLERINIT():
     dbclient = pymongo.MongoClient(f'mongodb://{DBIPADDRESS}:{DBPORT}/')
     
 
     dbcheck = dbclient.list_database_names()
     if DBNAME in dbcheck:
         LOGEVENTS_INFO(f"{DBNAME} Exists on mongodb server")
+
     else:
         serverdb = dbclient[f"{DBNAME}"]
+
 
         
 
