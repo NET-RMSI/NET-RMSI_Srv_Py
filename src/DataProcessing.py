9847b8094b82a33070a3f30852cfd41eab07b27f
Kitty Rose Hamilton <kittyrhamilton@gmail.com>
2022-11-05 16:56:13 +0000

    Bug fix

--- src/DataProcessing.py
+++ src/DataProcessing.py
@@ -4,18 +4,23 @@ from LoggerModule import LOGEVENTS_CRITICAL, LOGEVENTS_ERROR, LOGEVENTS_INFO
 def CLIENTIDENTIF(cliconn, connaddress):
    
    # Check if client version is compatable with the server version
-   cliid = cliconn.recvmsg(4096)
+   cliconn.settimeout(10.0)
+   cliid = cliconn.recv(4096).decode('utf-8')
+   
    
    if cliid == f"{controllercli}":
       cliconn.send("valid".encode())
+      LOGEVENTS_INFO(f"Recieved: {cliid}")
       LOGEVENTS_INFO(f"{controllercli} at {connaddress} identified")
    elif cliid == f"{controlledcli}":
       cliconn.send("valid".encode())
+      LOGEVENTS_INFO(f"Recieved: {cliid}")
       LOGEVENTS_INFO(f"{controlledcli} at {connaddress} identified")
 
       
    else:
       cliconn.send("invalid".encode())
+      LOGEVENTS_INFO(f"Recieved: {cliid}")
       LOGEVENTS_CRITICAL("Failed to recieve client identifier, unknown client")
       LOGEVENTS_CRITICAL(f"Closing connection to {connaddress}")
       cliconn.close()
