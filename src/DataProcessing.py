b107937a714ff261e9716d18ad960f51b6ca2b21
Kitty Rose Hamilton <kittyrhamilton@gmail.com>
2022-11-05 15:45:07 +0000

    Code refactoring, finalising testing of cli identification

--- src/DataProcessing.py
+++ src/DataProcessing.py
@@ -3,18 +3,19 @@ from LoggerModule import LOGEVENTS_CRITICAL, LOGEVENTS_ERROR, LOGEVENTS_INFO
 
 def CLIENTIDENTIF(cliconn, connaddress):
    
-   
-   with cliconn:
-      cliconn.send(f"{serverversion.encode()}")
-      cliid = cliconn.recvmsg(4096)
+   # Check if client version is compatable with the server version
+   cliid = cliconn.recvmsg(4096)
    
    if cliid == f"{controllercli}":
+      cliconn.send("valid".encode())
       LOGEVENTS_INFO(f"{controllercli} at {connaddress} identified")
    elif cliid == f"{controlledcli}":
+      cliconn.send("valid".encode())
       LOGEVENTS_INFO(f"{controlledcli} at {connaddress} identified")
 
       
    else:
+      cliconn.send("invalid".encode())
       LOGEVENTS_CRITICAL("Failed to recieve client identifier, unknown client")
       LOGEVENTS_CRITICAL(f"Closing connection to {connaddress}")
       cliconn.close()
