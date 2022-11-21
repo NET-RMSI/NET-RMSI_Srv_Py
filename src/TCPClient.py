5b7919648663e38fec2d6edeb3932eb4ec0c7052
Kitty Rose Hamilton <kittyrhamilton@gmail.com>
2022-11-21 13:57:38 +0000

    Update TCPClient.py

--- src/TCPClient.py
+++ src/TCPClient.py
@@ -1,4 +1,5 @@
 import threading
+from LoggerModule import *
 
 class TCPClient(threading.Thread):
    def __init__(self, ipaddress, port, connection):
@@ -9,7 +10,34 @@ class TCPClient(threading.Thread):
       
       self.connection = connection
       
-   def run(self):
+      self.clientidentif()
+      
+   def clientidentif(self):
+       while True:
+           
+        self.connection.settimeout(10.0)
+        indata = self.connection.recv(4096).decode('utf-8')
+       
+        if indata == f"{controllercli}":
+            self.connection.send("valid".encode())
+            LOGEVENTS_INFO(f"Recieved: {indata}")
+            LOGEVENTS_INFO(f"{controllercli} at {self.ipaddress} identified")
+            #CTRLHANDLING(cliconn)
+
+        elif indata == f"{controlledcli}":
+            self.connection.send("valid".encode())
+            LOGEVENTS_INFO(f"Recieved: {indata}")
+            LOGEVENTS_INFO(f"{controlledcli} at {self.ipaddress} identified")
+            #CTRLDHANDLING(cliconn, cliaddress)
+      
+        else:
+            self.connection.send("invalid".encode())
+            LOGEVENTS_INFO(f"Recieved: {indata}")
+            LOGEVENTS_CRITICAL("Failed to recieve client identifier, unknown client")
+            LOGEVENTS_CRITICAL(f"Closing connection to {self.ipaddress}")
+            #cliconn.close()
+            
+       
        
        
        
