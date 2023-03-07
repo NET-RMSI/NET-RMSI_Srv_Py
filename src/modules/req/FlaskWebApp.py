86485bb703e6361af95ec2cdf67f524130cb3841
Kitty Rose Hamilton <kittyrhamilton@gmail.com>
2023-03-07 22:56:00 +0000

    Update

--- src/modules/req/FlaskWebApp.py
+++ src/modules/req/FlaskWebApp.py
@@ -1,21 +1,14 @@
-from flask import Flask, render_template
-from flask import request
+from flask import Flask, render_template, request 
 from modules.req.EventLogging import *
 
 webapp = Flask(__name__)
 
-@webapp.route("/")
+@webapp.route("/", methods=["GET", "POST"])
 def index():
-    processuserinput(request.args.get("serverlist"))
+    print(request.form.get("serverip"))
     
     return render_template('index.html')
 
-def processuserinput(userinput):
-    if(userinput == ""):
-        LOGEVENTS_DEBUG("Nothing recieved from webapp")
-    
-    else:
-        LOGEVENTS_DEBUG("Data recieved: " + userinput)
         
         
     
