2083df9cab3aae14e2a00f59c8c7fad33fa28002
Kitty Rose Hamilton <kittyrhamilton@gmail.com>
2023-03-08 22:33:54 +0000

    Update

--- src/modules/req/FlaskWebApp.py
+++ src/modules/req/FlaskWebApp.py
@@ -2,12 +2,13 @@ from flask import Flask, render_template, request
 from modules.req.EventLogging import *
 
 webapp = Flask(__name__)
+test = "test"
 
 @webapp.route("/", methods=["GET", "POST"])
 def index():
     print(request.form.get("serverip"))
     
-    return render_template('index.html')
+    return render_template('index.html', serverip=test)
 
         
         
